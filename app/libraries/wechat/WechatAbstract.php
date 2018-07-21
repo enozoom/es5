48f1f04323e30e70e9a7b243337fcb7255bf333a
Joe <e@enozoom.com>
2018-07-21 09:25:17 +0800

    upgrade:更新accessToken代码

--- app/libraries/wechat/WechatAbstract.php
+++ app/libraries/wechat/WechatAbstract.php
@@ -21,17 +21,18 @@ abstract class WechatAbstract
     public function accessToken():string
     {
         $key = 'accessToken';
+        $token = '';
         if(!empty($accessToken = $this->Cache->getItemByKey($key))){
-            return $accessToken;
+            $token = $accessToken;
+        }else{
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s';
+            $json = json_decode($this->curlGet(sprintf($url,$this->appid,$this->appSecret)));
+            if(empty($json->errcode)){
+                 $this->Cache->setItem($key, $json->access_token,$json->expires_in);
+                 $token = $json->access_token;
+            }
         }
-        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s';
-        $json = json_decode($this->curlGet(sprintf($url,$this->appid,$this->appSecret)));
-        if(!empty($json->errcode)){
-            var_dump($json);exit();
-            return '';
-        }
-        $this->Cache->setItem($key, $json->access_token,$json->expires_in);
-        return $json->access_token;
+        return $token;
     }
     
     protected function log($msg){
