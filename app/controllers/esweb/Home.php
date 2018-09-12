3d480fc9bd6d66c4a3dbfa082c7c769463ead953
Joe <e@enozoom.com>
2018-09-12 15:03:40 +0800

    使用新命名公共样式

--- app/controllers/esweb/Home.php
+++ app/controllers/esweb/Home.php
@@ -6,7 +6,7 @@ use ES\Core\Toolkit\ConfigStatic;
 
 final class Home extends HtmlController
 {
-    public $css = 'public.base.min';
+    public $css = 'public.common.reset';
     public function index(){
         $this->view(['v'=>substr(ConfigStatic::getConfig('version','Param'),0,4)]);
     }
